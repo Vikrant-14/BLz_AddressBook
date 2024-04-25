695c97366362b646dcf06eb9fc5bf3d0e5acb8bc
Vikrant-14 <vikrantgawale123@gmail.com>
2024-04-25 17:38:00 +0530

    [Vikrant] Add : Created a Contact class consisting all the required fields and added record using user input

--- AddressBook/obj/Debug/net8.0/AddressBook.AssemblyInfo.cs
+++ AddressBook/obj/Debug/net8.0/AddressBook.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("AddressBook")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d9687cd6a96d421477afdc2abedbf39facea0fc9")]
 [assembly: System.Reflection.AssemblyProductAttribute("AddressBook")]
 [assembly: System.Reflection.AssemblyTitleAttribute("AddressBook")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
