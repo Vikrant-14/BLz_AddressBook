2c5a54f36de018afcea989caa97eb4b58eca5b10
Vikrant-14 <vikrantgawale123@gmail.com>
2024-04-25 17:58:06 +0530

    [Vikrant] Add : User can add Multiple Records in AddressBook

--- AddressBook/AddressBookMain.cs
+++ AddressBook/AddressBookMain.cs
@@ -126,7 +126,6 @@ namespace Assignment3
             {
                 switch (choice)
                 {
-
                     case 1:
                         Console.WriteLine("Add Record\n--------------");
 
