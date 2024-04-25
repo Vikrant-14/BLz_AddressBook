d9687cd6a96d421477afdc2abedbf39facea0fc9
Vikrant-14 <vikrantgawale123@gmail.com>
2024-04-25 17:31:19 +0530

    [Vikrant] Add : Created a AddressBook class displaying welcome message

---
+++ AddressBook/AddressBookMain.cs
@@ -0,0 +1,22 @@
+﻿using AddressBook;
+using System;
+
+namespace Assignment3
+{
+    class AddressBookMain
+    {
+        List<Contact>? ContactList;
+
+        static AddressBookMain()
+        {
+            Console.WriteLine("================================");
+            Console.WriteLine("Welcome to Address Book Program");
+            Console.WriteLine("================================\n");
+        }
+
+        public static void Main()
+        {
+
+        }
+    }
+}
